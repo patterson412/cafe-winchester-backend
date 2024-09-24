cc45f64f96e4def425c15624ecdd0c3ea1638537
patterson412 <patterson.leon1960@gmail.com>
2024-09-25 01:21:54 +0530

    Initial Spring project structure

---
+++ src/main/java/com/projects/cafe_winchester_backend/CafeWinchesterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projects.cafe_winchester_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeWinchesterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeWinchesterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/cafe_winchester_backend/CafeWinchesterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.cafe_winchester_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeWinchesterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
