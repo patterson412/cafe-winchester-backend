eeccd714e430c31efe8bf464330fdd6e293c54ad
patterson412 <patterson.leon1960@gmail.com>
2024-11-20 02:57:29 +0530

    commented for clarity

--- src/main/java/com/projects/cafe_winchester_backend/entity/Role.java
+++ src/main/java/com/projects/cafe_winchester_backend/entity/Role.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "roles")
 public class Role {
     @EmbeddedId
-    private RoleId id; // Has userId and role
+    private RoleId id; // Has userId and role. Acts as the Composite key
 
     @ManyToOne
     @MapsId("userId") // Reuses the userId from the composite key through the RoleId class.  ← Maps to RoleId.userId field. Avoids duplicating user_id column
